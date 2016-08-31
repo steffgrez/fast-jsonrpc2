65330a298f2805de39630c7b58de315e784d266f
Stephane LAHACHE <slahache@gmail.com>
2016-08-31 14:09:14 +0200

    [QUALITY] add pylint rules, and optimize score

--- fast_jsonrpc2/response.py
+++ fast_jsonrpc2/response.py
@@ -9,21 +9,21 @@ class ResponseHandler(object):
     def __init__(self, error_verbose=True):
         self.error_verbose = error_verbose
 
-    def get_response(self, result, id):
+    def get_response(self, result, request_id):
         return {
             "jsonrpc": "2.0",
             "result": result,
-            "id": id
+            "id": request_id
         }
 
-    def get_error_response(self, code, message, data=None, id=None):
+    def get_error_response(self, code, message, data=None, request_id=None):
         result = {
             "jsonrpc": "2.0",
             "error": {
                 "code": code,
                 "message": message,
             },
-            "id": id
+            "id": request_id
         }
 
         if self.error_verbose and data:
@@ -31,32 +31,32 @@ class ResponseHandler(object):
 
         return result
 
-    def get_parse_error_response(self, data=None, id=None):
+    def get_parse_error_response(self, data=None, request_id=None):
         return self.get_error_response(
-            -32700, 'Parse error', data=data, id=id
+            -32700, 'Parse error', data=data, request_id=request_id
         )
 
-    def get_invalid_request_response(self, data=None, id=None):
+    def get_invalid_request_response(self, data=None, request_id=None):
         return self.get_error_response(
-            -32600, 'Invalid Request', data=data, id=id
+            -32600, 'Invalid Request', data=data, request_id=request_id
         )
 
-    def get_method_not_foud_response(self, data=None, id=None):
+    def get_method_not_foud_response(self, data=None, request_id=None):
         return self.get_error_response(
-            -32601, 'Method not found', data=data, id=id
+            -32601, 'Method not found', data=data, request_id=request_id
         )
 
-    def get_invalid_params_response(self, data=None, id=None):
+    def get_invalid_params_response(self, data=None, request_id=None):
         return self.get_error_response(
-            -32602, 'Invalid params', data=data, id=id
+            -32602, 'Invalid params', data=data, request_id=request_id
         )
 
-    def get_internal_error_response(self, data=None, id=None):
+    def get_internal_error_response(self, data=None, request_id=None):
         return self.get_error_response(
-            -32603, 'Internal error', data=data, id=id
+            -32603, 'Internal error', data=data, request_id=request_id
         )
 
-    def get_server_error_response(self, data=None, id=None):
+    def get_server_error_response(self, data=None, request_id=None):
         return self.get_error_response(
-            -32000, 'Server error', data=data, id=id
+            -32000, 'Server error', data=data, request_id=request_id
         )
